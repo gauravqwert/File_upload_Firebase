eecbe4e71028def67f6a691a7158ca78a372b78e
unknown <patelgaurav282000@gmail.com>
2024-07-23 13:06:52 +0530

    Upload File in Firebase Using React Native

---
+++ firebaseConfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const storage =getStorage(app);
+export const db = getFirestore(app);
